203beeda6c55e328c29f5ea2985b25d8acc353b3
guasses <1270486190@qq.com>
2020-01-22 18:21:42 +0800

    完善功能

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,91 @@
 $(function(){
-    const indicator = document.querySelector('.nav-indicator');
-    const items = document.querySelectorAll('.nav-item');
-
-    function handleIndicator(el) {
-        items.forEach(item => {
-            item.classList.remove('is-active');
-            item.removeAttribute('style');
+    //$("#audioControl").hide();
+    //$("#save").hide();
+    var appKey = '7855455bc8c03449';
+    var key = 'BNjvoaZgbB2hZTVicE5DnLs0frYGQB2W';
+    var media = $('#media')[0];
+    var word = "";
+    var explains = "";
+    var audioUrl = "";
+    $("#translate").click(function(e){
+        word = $("#world").val();
+        //console.log(world);
+        var salt = (new Date).getTime();
+        var curTime = Math.round(new Date().getTime()/1000);
+        var query = word;
+        // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+        var from = 'en';
+        var to = 'zh-CHS';
+        var str1 = appKey + truncate(query) + salt + curTime + key;
+        //console.log(str1);
+        var sign = sha256(str1);
+        $.ajax({
+            url: 'http://openapi.youdao.com/api',
+            type: 'post',
+            dataType: 'jsonp',
+            data: {
+                q: query,
+                appKey: appKey,
+                salt: salt,
+                from: from,
+                to: to,
+                sign: sign,
+                signType: "v3",
+                curtime: curTime,
+            },
+            success: function (data) {
+                //console.log(data);
+                var explainsArray = data["basic"]["explains"];
+                explains = explainsArray.join(" ");
+                var tmp = explainsArray.join("<br>");
+                audioUrl = data["speakUrl"];
+                $('#media').html("<source src='"+audioUrl+"' type='audio/mpeg'>");
+                media.load();
+                $("#meaning").html("<p>"+tmp+"</p>");
+                $("#audioControl").show();
+                $("#save").show();
+            }
         });
-
-        indicator.style.width = `${el.offsetWidth}px`;
-        indicator.style.left = `${el.offsetLeft}px`;
-        indicator.style.backgroundColor = el.getAttribute('active-color');
-
-        el.classList.add('is-active');
-        el.style.color = el.getAttribute('active-color');
+    });
+    $("#save").click(function(e){
+        $.post('word',{word:word,explains:explains,audioUrl:audioUrl},function(data){
+            console.log(data);
+            if(data=="2"){
+                alert("该单词数据库中已存在");
+            }else if(data=="1"){
+                alert("插入成功！");
+            }
+        });
+    });
+    var audioTimer = null;
+    //console.log(media);
+    //绑定播放暂停控制
+    $('.play').bind('click', function() {
+        playAudio();
+    }); 
+    
+    //播放暂停切换
+    function playAudio() {
+        if(media.paused) {
+            play();
+        } else {
+            pause();
+        }
+    }
+    
+    //播放
+    function play() {
+        media.play();
+    }
+    
+    //暂停
+    function pause() {
+        media.pause();
     }
 
-    items.forEach((item, index) => {
-        item.addEventListener('click', e => {handleIndicator(e.target);});
-        item.classList.contains('is-active') && handleIndicator(item);
-    });
 });
+function truncate(q){
+    var len = q.length;
+    if(len<=20) return q;
+    return q.substring(0, 10) + len + q.substring(len-10, len);
+}
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const request = require('request');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const fs = require('fs');
+var path = require("path");
 const bodyParser = require('body-parser');
 const BaseModel = require('./js/base_model.js');
 
@@ -10,9 +12,31 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 server.listen(80,()=>{
     console.log('端口80已运行web服务!');
 });
-app.use(express.static('public'));
-
 var baseModel = new BaseModel();
+app.post('/word',urlencodedParser,function(req,res){
+    //console.log(req.body);
+    let word = req.body.word;
+    let explains = req.body.explains;
+    let audioUrl = req.body.audioUrl;
+    var filename = audioUrl.match(/q=.+&voice/);
+    //console.log(filename);
+    var stream = fs.createWriteStream("./public/audio/"+filename+".mp3");
+    request(audioUrl).pipe(stream).on("close",function(err){
+        console.log("文件["+filename+"]下载完毕");
+    });
+    baseModel.findOneById('word',{'word':word},function(result){
+        if(result){
+            res.end("2");
+        }else{
+            baseModel.insert('word',{'word':word,'explains':explains,'audioUrl':filename},function(result){
+                if(result){
+                    res.end("1");
+                }
+            });
+        }
+    });
+});
+app.use(express.static('public'));
 
 app.post('/select-head',urlencodedParser,(req,res,next)=>{
     let user_id = req.body.user_id;
