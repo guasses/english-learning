d16deecc4d383c14800453682111995d5c9b8723
guasses <1270486190@qq.com>
2020-01-23 21:20:28 +0800

    完善功能

--- public/js/word.js
+++ public/js/word.js
@@ -1,27 +1,23 @@
 $(function(e){
     var $t = $('#t');
+    var media = $('#media')[0];
     $.post('wordList',{date:"new"},function(data){
         let t = JSON.parse(data);
         t.forEach(e => {
-            $t.append("<ul class='list-group' id='list'><li class='list-group-item'>"+
+            $t.append("<ul class='list-group list'><li class='list-group-item'>"+
                 "<h4 class='list-group-item-heading'>"+e['word']+"</h4><div class='audioControl'>"+
-                "<div class='play'><span id='play' class='glyphicon glyphicon-volume-up'></span>"+
                 "</div></div><p class='list-group-item-text'>"+e['explains']+"</p><span class='none'>"+
                 e['audioUrl']+"</span></li></ul>");
+        });
+        $('.list-group-item').each(function(i){
+            $(this).click(function(e){
+                let t = $(this).find('.none').text();
+                let audioUrl = "./audio/"+t+".mp3";
+                $('#media').html("<source src='"+audioUrl+"' type='audio/mpeg'>");
+                media.load();
+                media.play();
+            });
         });
     });
 });
 
-
-/*<ul class="list-group" id="list">
-                    <li class="list-group-item">
-                        <h4 class="list-group-item-heading">List group item heading</h4>
-                        <div class="audioControl">
-                            <div class="play">
-                                <span id="play" class="glyphicon glyphicon-volume-up"></span>
-                            </div>
-                        </div>
-                        <p class="list-group-item-text">11111111</p>  
-                        <span class="none"></span>   
-                    </li>
-                </ul>*/
